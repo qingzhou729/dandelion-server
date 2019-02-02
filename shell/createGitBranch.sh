505d814a73341abfd3968609b8b7f54d2703d931
yuxue.yang <yuxue.yang@husor.com>
2019-02-02 15:39:03 +0800

    test

--- shell/createGitBranch.sh
+++ shell/createGitBranch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
-git checkout -b test_jc
-git push --set-upstream origin test_jc
+git checkout -b test_jc1
+git push --set-upstream origin test_jc1
 
 git merge test_me
 git push
