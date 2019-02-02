fb542629e8190b3267e4d8f47ff2fb3be7b61a03
yuxue.yang <yuxue.yang@husor.com>
2019-02-02 12:06:42 +0800

    增加线上数据库配置

--- config/dbConf.js
+++ config/dbConf.js
@@ -6,5 +6,13 @@ const DEV = {
     port: '3306',        //端口
     host: '127.0.0.1'     //服务ip地址
 }
-dbConf = DEV;
+
+const PRO = {
+    database: 'dandelion',    //数据库
+    user: 'root',    //用户
+    password: '123456',     //密码
+    port: '3306',        //端口
+    host: '47.107.188.55'     //服务ip地址
+}
+dbConf = PRO;
 module.exports = dbConf;
