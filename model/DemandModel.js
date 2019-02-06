14b65bb92aa4274b964b3dd81d7c52a0536e4b73
yuxue.yang <yuxue.yang@husor.com>
2019-02-06 18:46:30 +0800

    ad

--- model/DemandModel.js
+++ model/DemandModel.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2019-01-30 22:29:34
  * @LastEditors: yuxue.yang
- * @LastEditTime: 2019-01-31 13:26:48
+ * @LastEditTime: 2019-02-02 22:04:48
  */
 
 const {query} = require('../common/mysql');
@@ -17,11 +17,10 @@ class DemandModel {
     async insertDemand(params) {
         console.log(params)
         const sql = 'insert demand_info (did, title, demand_desc, status, uid) values(?,?,?,?,?)';
-        
         let data = await query(sql, params, (err, result) => {
             return result;
         });
-        return data; 
+        return data;
     }
 
     /**
@@ -30,7 +29,9 @@ class DemandModel {
      * @return: 用户的所有需求
      */
     async selectDemandByUid(uid) {
-        const sql = `SELECT * FROM demand_info WHERE uid=${uid}`;
+        const sql = `SELECT a.did, a.title, a.demand_desc, a.status, a.bid, b.branch_name from demand_info a LEFT JOIN branch_info b
+on a.bid = b.bid WHERE uid=${uid}`;
+        // const sql = `SELECT * FROM demand_info WHERE uid=${uid}`;
         const data = await query(sql);
         return data;
     }
