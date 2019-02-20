0665f3c2c4f90858e92111fb4358a04212b98335
yuxue.yang <yuxue.yang@husor.com>
2019-02-20 13:01:40 +0800

    f

--- shell/stage.sh
+++ shell/stage.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# 进入目标项目
+echo '进入目标项目'
 cd $1
+echo '切换到开发分支'
 git checkout $2
+echo 'pull 所有代码'
 git pull origin $2
+echo '删除node_modules'
 rm -rf node_modules/
+echo 'npm install'
 npm install
+echo 'npm run build'
 npm run build
-
-# git checkout master
-# git pull origin master
-# git merge $2 -m '预发'
-
 
